585c7420f2733202829a6695a2641bb49febb101
Martin <martin.kotek.317066@gmail.com>
2026-05-22 22:23:49 +0200

    update backend frontend

--- backend/main.py
+++ backend/main.py
@@ -68,24 +68,36 @@ def set_cache(key, data):
 
 async def safe_get(client, url, params=None):
     try:
+
         headers = {
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
-            "Accept": "application/json,text/plain,*/*",
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/124.0 Safari/537.36"
+            ),
+            "Accept": "application/json",
+            "Connection": "keep-alive",
         }
 
         r = await client.get(
             url,
             params=params,
-            timeout=17,
             headers=headers,
+            timeout=20,
+            follow_redirects=True,
         )
 
+        print("URL:", url)
+        print("STATUS:", r.status_code)
+
         if r.status_code != 200:
+            print("BODY:", r.text[:500])
             return None
 
         return r.json()
 
-    except:
+    except Exception as e:
+        print("SAFE_GET ERROR:", repr(e))
         return None
 
 # =========================================================
