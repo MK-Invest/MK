ab1827bda47ace335453492f779886ebbcff4940
Martin <martin.kotek.317066@gmail.com>
2026-05-31 12:44:06 +0200

    update dashboard v2

--- frontend/stocklens-react/src/main.jsx
+++ frontend/stocklens-react/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500;600;700&display=swap" rel="stylesheet">
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
